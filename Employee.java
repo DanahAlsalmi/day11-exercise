2016c6f7503713cee4c001a85b68fd9759f52e85
Danah alsalmi <118121745+DanahAlsalmi@users.noreply.github.com>
2024-07-14 17:10:39 +0300

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,52 @@
+public class Employee {
+    private String name;
+    private String id;
+    private int salary;
+
+    public Employee(String name, String id, int salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAnnualSalary(){
+        return salary * 12;
+    }
+    public int raisedSalary(int percent){
+        salary += salary * percent / 100;
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ Main.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("~~~~ Welcome to our System :) ~~~~");
+//        System.out.println("------------------------------------------------");
+//        System.out.println("Enter details for first account:");
+//        System.out.print("ID: ");
+//        String id1 = input.nextLine();
+//        System.out.print("Name: ");
+//        String name1 = input.nextLine();
+//        Account acc1 = new Account(id1, name1);
+//
+//        System.out.println("------------------------------------------------");
+//        System.out.println("\nEnter details for second account:");
+//        System.out.print("ID: ");
+//        String id2 = input.nextLine();
+//        System.out.print("Name: ");
+//        String name2 = input.nextLine();
+//        System.out.print("Initial Balance: ");
+//        int balance2 = input.nextInt();
+//        Account acc2 = new Account(id2, name2, balance2);
+//
+//        System.out.println("------------------------------------------------");
+//        System.out.println("\nEnter details for third account:");
+//        input.nextLine();
+//        System.out.print("ID: ");
+//        String id3 = input.nextLine();
+//        System.out.print("Name: ");
+//        String name3 = input.nextLine();
+//        System.out.print("Initial Balance: ");
+//        int balance3 = input.nextInt();
+//        Account acc3 = new Account(id3, name3, balance3);
+//
+//        while (true) {
+//            System.out.println("\nChoose an operation:");
+//            System.out.println("1. Credit to " + acc1.getName() + "'s account");
+//            System.out.println("2. Debit from " + acc2.getName() + "'s account");
+//            System.out.println("3. Transfer from " + acc3.getName() + "'s account to " + acc1.getName() + "'s account");
+//            System.out.println("4. Display account details");
+//            System.out.println("5. Exit");
+//            System.out.print("Enter your choice: ");
+//            int choice = input.nextInt();
+//
+//            switch (choice) {
+//                case 1:
+//                    System.out.print("Enter amount to credit to " + acc1.getName() + "'s account: ");
+//                    int creditAmount = input.nextInt();
+//                    acc1.credit(creditAmount);
+//                    System.out.println("------------------------------------------------");
+//                    System.out.println("After crediting " + creditAmount + " to " + acc1.getName() + "'s account:");
+//                    System.out.println(acc1);
+//                    System.out.println("------------------------------------------------");
+//
+//                    break;
+//                case 2:
+//                    System.out.print("Enter amount to debit from " + acc2.getName() + "'s account: ");
+//                    int debitAmount = input.nextInt();
+//                    acc2.debit(debitAmount);
+//                    System.out.println("------------------------------------------------");
+//                    System.out.println("After debiting " + debitAmount + " from " + acc2.getName() + "'s account:");
+//                    System.out.println(acc2);
+//                    System.out.println("------------------------------------------------");
+//
+//                    break;
+//                case 3:
+//                    System.out.print("Enter amount to transfer from " + acc3.getName() + "'s account to " + acc1.getName() + "'s account: ");
+//                    int transferAmount = input.nextInt();
+//                    acc1.transferTo(acc3, transferAmount);
+//                    System.out.println("------------------------------------------------");
+//                    System.out.println("After transferring " + transferAmount + " from " + acc3.getName() + "'s account to " + acc1.getName() + "'s account:");
+//                    System.out.println(acc3);
+//                    System.out.println(acc1);
+//                    System.out.println("------------------------------------------------");
+//
+//                    break;
+//                case 4:
+//                    System.out.println("Account Details:");
+//                    System.out.println(acc1);
+//                    System.out.println("------------------------------------------------");
+//                    System.out.println(acc2);
+//                    System.out.println("------------------------------------------------");
+//                    System.out.println(acc3);
+//                    break;
+//                case 5:
+//                    System.out.println("Exiting the program.");
+//                    return;
+//                default:
+//                    System.out.println("Invalid choice. Please try again.");
+//            }
+//        }
+
+
+//        Employee em=new Employee("Dana","1234",20000);
+//
+//
+//        System.out.println(em);
+//        System.out.println("Annual Salary: " + em.getAnnualSalary());
+//        System.out.println("------------------------------------------------");
+//
+//        em.raisedSalary(10);
+//
+//        System.out.println(em);
+//        System.out.println("New Annual Salary: " + em.getAnnualSalary());
+    }
+}
